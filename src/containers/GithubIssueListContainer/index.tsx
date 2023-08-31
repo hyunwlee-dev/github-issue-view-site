6531001b3fc227de89e6ce88d965065519dad856
hyunwlee-dev <hwlee3928@gmail.com>
2023-08-31 22:51:09 +0900

    :sparkles: Feat: using Intersection Observer API in issueList

--- src/containers/GithubIssueListContainer/index.tsx
+++ src/containers/GithubIssueListContainer/index.tsx
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { IssueItem } from '../../components/IssueItem';
 import { useIssueList } from '../../hooks/useIssueList';
 import { Issue } from '../../types/issue';
 import * as S from './GithubIssueListContainer.style';
 
 export const GithubIssueListContainer = () => {
-  const { issueList, isLoading, error } = useIssueList();
-  if (isLoading) {
-    return <>Loading...</>;
-  }
+  const target = useRef<any>('');
+
+  useEffect(() => {
+    observer.observe(target?.current);
+  }, []);
+
+  const options = {
+    threshold: 1.0,
+  };
+
+  const callback = () => {
+    refetch();
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+  const { issueList, isLoading, error, refetch, page } = useIssueList();
   if (error) {
     return <>Error!</>;
   }
   return (
     <S.IssueList>
+      <button onClick={refetch}>{page}</button>
+      {isLoading}
       {issueList?.map(({ id, number, title, user, created_at, comments }: Issue) => (
         <IssueItem
           key={id}
           number={number}
           title={title}
-          login={user.login}
+          login={user?.login}
           created_at={created_at}
           comments={comments}
         />
       ))}
+      {isLoading && <>loading...</>}
+      <div ref={target} />
     </S.IssueList>
   );
 };
